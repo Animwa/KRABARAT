7846af0a585c1d2cf3465f241d4d74612420080d
arifin05 <107057460+Animwa@users.noreply.github.com>
2026-09-19 21:14:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // FRONTEND LOGIC & INTEGRASI REST API KARANGANYAR BARAT (FULL UPDATED & ANTI DUPLIKAT)
 // ==========================================
 
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwdXlbzVlxPjo7B_OBSdCF9knc8c9ObeijHCixMBSQh6zidRUcSjsaOSTUALfm2urJY/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzciFcbS9cbcYpEuaohamvozKneSW46eFGskkB-1FPczQ5c_2fJQwB2Pko9GziDl4Mu/exec";
 
 let appData = {
   pengurus: [],
